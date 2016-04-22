ce16ccb5c809c0c9acd77d9ffd58079d91f13af8
Jane Huston <jehuston@users.noreply.github.com>
2016-04-22 12:05:58 -0700

    templates added

--- my_app/app.py
+++ my_app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 from pymongo import MongoClient
 from recommender import MyRecommender
 
